87c41db9b6ea1b7fdfd211bbfd754d3eab90f705
Shanka Visal <shankavisal@gmail.com>
2023-10-28 01:50:11 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {Maincomponent} from './main/main.component';
 import { main2Component } from './main2/main2.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Maincomponent,
-    main2Component
+    main2Component,
+    NavigationComponent,
+    BarComponent
   ],
   imports: [
     BrowserModule,
